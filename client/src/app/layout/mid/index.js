bcbb4a2cb1b482edff879225b756d6020d39751d
achbj <bj.ach7@gmail.com>
2022-06-10 07:49:59 +0545

    project setup client

---
+++ client/src/app/layout/mid/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Router from "../../routes";
+
+class Mid extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="mid">
+          <Router />
+        </div>
+      </>
+    );
+  }
+}
+
+export default Mid;
---
+++ client/src/app/pages/auction/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Auction extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="Auction">
+          <h1>Auction</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Auction;
