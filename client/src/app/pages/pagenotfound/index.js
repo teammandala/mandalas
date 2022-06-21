a72259886d9f670867adfdbb8649aa89ae2d5723
sagarchaulagain <41198178+sagarchaulagain@users.noreply.github.com>
2022-06-21 15:36:43 +0545

    changes made again after upload by banish

--- client/src/app/pages/pagenotfound/index.js
+++ client/src/app/pages/pagenotfound/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import { Button, Result } from 'antd';
 const Pagenotfound = () => {
   return (
     <div>
-        <h1>404 Error</h1>
-        <h1>Pagenotfound</h1>
+        <Result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+    extra={<Button href='/' type="primary">Back Home</Button>}
+  />
     </div>
   )
 }
