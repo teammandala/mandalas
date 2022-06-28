d7948b37afdad8db62e90b285dd88451e7b5099f
wrishav1 <99461741+wrishav1@users.noreply.github.com>
2022-06-28 09:27:30 +0545

    Update user.js

--- client/src/app/api/user.js
+++ client/src/app/api/user.js
@@ -6,7 +6,7 @@ const API_URL = "http://localhost:8080/api/auth/";
 const token = localStorage.getItem("token");
 
 // register user
-const register = (username, name, email, phone, password) => {
+const register = (username, name, email, phone, password, password1) => {
   const config = {
     headers: { "Content-Type": "application/json" },
   };
@@ -19,6 +19,7 @@ const register = (username, name, email, phone, password) => {
       email,
       phone,
       password,
+      password1,
     },
     config
   );
@@ -49,6 +50,7 @@ const login = (username, password) => {
     });
 };
 
+
 //current user data
 const getCurrentUser = () => {
   return JSON.parse(localStorage.getItem("user"));
@@ -59,35 +61,55 @@ const logout = () => {
   localStorage.removeItem("user");
 };
 
-const updateUser = (
-  usernameParams,
-  username,
-  email,
-  phone,
-  address,
-  bio,
-  avatar
-) => {
+const updateUser = (username, email, name, phone, address, bio) => {
+  const currentUser = getCurrentUser();
   return axios
     .put(
-      API_URL + "profile/" + usernameParams,
+      API_URL + "profile/" + currentUser._id,
+      {
+        username,
+        email,
+        name,
+        phone,
+        address,
+        bio,
+      },
+      {
+        headers: {
+          // "Content-Type": "applicatoin/json",
+          //     Accept: "application/x-www-form-urlencoded",
+          //     mimeType: "multipart/form-data",
+          "x-access-token": token,
+        },
+      }
+    )
+    .then((res) => {
+      localStorage.clear("user");
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+      // console.log(res.data.user);
+      return res.data;
+    });
+};
+
+const updateUserAvatar = (avatar) => {
+  const currentUser = getCurrentUser();
+  return axios
+    .post(
+      API_URL + "avatar/" + currentUser._id,
+      {
+        avatar,
+      },
       {
         headers: {
           "Content-Type": "multipart/form-data",
           "x-access-token": token,
         },
-      },
-      {
-        username,
-        email,
-        phone,
-        address,
-        bio,
-        avatar,
       }
     )
     .then((res) => {
+      localStorage.clear("user");
       localStorage.setItem("user", JSON.stringify(res.data.user));
+      // console.log(res.data.user);
       return res.data;
     });
 };
@@ -99,4 +121,5 @@ export default {
   getCurrentUser,
   logout,
   updateUser,
+  updateUserAvatar,
 };
