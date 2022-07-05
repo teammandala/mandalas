8a3f219f854f434c56f2f8d013adc08e6d9d2c8c
achbj <bj.ach7@gmail.com>
2022-07-05 21:29:01 +0545

    date in auction detail page

--- client/src/app/components/auctionbid/index.js
+++ client/src/app/components/auctionbid/index.js
@@ -1,60 +1,52 @@
-
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import auction from '../../api/auction'
-import Pagenotfound from '../../pages/pagenotfound';
-import './style.css'
-
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import auction from "../../api/auction";
+import moment from "moment";
+import Pagenotfound from "../../pages/pagenotfound";
+import "./style.css";
 
 const BiddingPage = () => {
-   const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   const id = useParams().id;
 
   useEffect(() => {
-  
-    auction.
-    getCurrentAuction(id).then( res => {
-    const data = res.data.currentauctionData;
-    setData(data);
-    console.log(data)
-
-    }).catch( error => {
-      console.log(error);
-    });
+    auction
+      .getCurrentAuction(id)
+      .then((res) => {
+        const data = res.data.currentauctionData;
+        setData(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
-
-
 
   return (
     <>
-
-      <div className='container-fluid'>
+      <div className="container-fluid">
         <h1>auctions bid</h1>
         <h2>{data.description}</h2>
         <div>
-          <img className='bid-image'
-          src={"http://localhost:8080/" + data.image} alt="" />
-
+          <img
+            className="bid-image"
+            src={"http://localhost:8080/" + data.image}
+            alt=""
+          />
         </div>
-        <div className='container-fluid'>
+        <div className="container-fluid">
           {data.itemName}
-          <br/>
+          <br />
           {data.updated}
-          {data.bidStart}
-          {data.bidEnd}
+          {moment(data.bidStart).format("DD/MM/YYYY")}
+          {moment(data.bidEnd).format("DD/MM/YYYY")}
           {data.seller}
           {data.startingBid}
         </div>
-        <div>
-          {data.bids}
-        </div>
+        <div>{data.bids}</div>
       </div>
     </>
-  )
+  );
+};
 
-
-
-}
-
-export default BiddingPage
+export default BiddingPage;
