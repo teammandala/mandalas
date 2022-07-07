7a550ab5a55531afcb7b1b17c56b08e6487a7e21
Sagar Prasad Chaulagain <41198178+sagarchaulagain@users.noreply.github.com>
2022-07-07 15:36:04 +0545

    bid buddto added

--- client/src/app/components/auctionbid/index.js
+++ client/src/app/components/auctionbid/index.js
@@ -4,6 +4,7 @@ import auction from "../../api/auction";
 import moment from "moment";
 import Pagenotfound from "../../pages/pagenotfound";
 import "./style.css";
+import {Button, Form, Input} from 'antd'
 
 const BiddingPage = () => {
   const [data, setData] = useState([]);
@@ -24,26 +25,59 @@ const BiddingPage = () => {
 
   return (
     <>
-      <div className="container-fluid">
-        <h1>auctions bid</h1>
-        <h2>{data.description}</h2>
-        <div>
+      <div className="container-fluid auction-bid">
+        <div className="row">
+        <h4>{data.description}</h4>
+        </div>
+        <div className="row">
           <img
             className="bid-image"
             src={"http://localhost:8080/" + data.image}
             alt=""
           />
+        <div className="bid row">
+          <Form >
+            <Form.Item
+            label='amount'
+            >
+              <Input type='number' placeholder='enter amount'/>
+              
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Bid
+              </Button>
+            </Form.Item>
+          </Form>
+            
+          </div>
+          <div>{data.bids}</div>
+        
+          
+
+          <div className="container-fluid">
+            <div className="row">
+            <div className="row">
+              <h1>Product Name: {data.itemName}</h1>
+            </div>
+            <div className="col">
+              <h3>Last Updated: {data.updated}</h3>
+            </div>
+            <div className="col">
+              <h2>Starting Price: {data.startingBid}</h2>
+              </div>
+            <div className="col">
+              <h3>Started: {moment(data.bidStart).format("DD/MM/YYYY")}</h3>
+            </div>
+            <div className="col">
+              <h3>Ends At: {moment(data.bidEnd).format("DD/MM/YYYY")}</h3>
+            </div>
+            <div className="row">
+            <p>Sold by: <strong>{data.seller}</strong> </p> 
+            </div>
+          </div>
+          </div>
         </div>
-        <div className="container-fluid">
-          {data.itemName}
-          <br />
-          {data.updated}
-          {moment(data.bidStart).format("DD/MM/YYYY")}
-          {moment(data.bidEnd).format("DD/MM/YYYY")}
-          {data.seller}
-          {data.startingBid}
-        </div>
-        <div>{data.bids}</div>
       </div>
     </>
   );
