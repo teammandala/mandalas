9b3e1f49aa31a68c1468dc4554d458c2ba9ac265
achbj <bj.ach7@gmail.com>
2022-07-07 16:02:20 +0545

    contact page

--- client/src/app/pages/contact/index.js
+++ client/src/app/pages/contact/index.js
@@ -1,15 +1,23 @@
 import React from "react";
+// import { Link } from 'react-router-dom';
 import ContactForm from "../../components/form/contact";
-class Contact extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="contact">
-        <ContactForm />
+import "./style.css";
+
+function Contact() {
+  return (
+    <>
+      <div className="contact-page">
+        <div className="contact-box">
+          <div className="illustration-wrapper">
+            <h1>Contact Us</h1>
+            <br />
+            <h4>Our Email</h4>
+          </div>
+          <ContactForm />
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default Contact;
