7f03a9cb1068d7ed224ab3a8282ad434d5b97f3c
Sagar Prasad Chaulagain <41198178+sagarchaulagain@users.noreply.github.com>
2022-07-09 19:08:53 +0545

    winner data got

--- client/src/app/components/auction/completedauctions/index.js
+++ client/src/app/components/auction/completedauctions/index.js
@@ -6,11 +6,11 @@ import { Table } from "react-bootstrap";
 import moment from "moment";
 
 const Completedauctions = () => {
-  const [data, setData] = useState([]);  
-  
+  const [data, setData] = useState([]);
+
   const [id, setId] = useState("");
   const [isWinnerModalVisible, setIsWinnerModalVisible] = useState(false);
-  
+
   const [bidData, setBidData] = useState([]);
   const showWinnerModal = (items) => {
     setId(items._id);
@@ -24,7 +24,7 @@ const Completedauctions = () => {
       .getAuctionBySeller(cuser._id)
       .then((res) => {
         const data = res.data.currentauctionData;
-        setData(data);        
+        setData(data);
         setBidData(data.bids.sort().reverse());
         console.log(bidData);
       })
@@ -38,9 +38,6 @@ const Completedauctions = () => {
     setIsWinnerModalVisible(false);
     setIsWinnerModalVisible(false);
   };
-  
-
-  
 
   return (
     <>
@@ -64,52 +61,51 @@ const Completedauctions = () => {
             <th>Ending Time</th>
             <th>starting Price</th>
             <th>Winner</th>
-            
           </tr>
         </thead>
         <tbody>
           {data.map((items, index) => {
-            if(items.status === "approved"){
-                if (
-                    currentDate > new Date(items.bidEnd)
-                ){
-                    return (
-                        <tr>
-                          <td>{items._id}</td> <td>{items.itemName}</td>
-                          <td>{items.description}</td>
-                          <td>
-                            <Image
-                              width={200}
-                              src={"http://localhost:8080/" + items.image}
-                            />
-                          </td>
-                          <td>{moment(items.created).format("YYYY/MM/DD-HH:mm:ss")}</td>
-                          <td>
-                            {moment(items.bidStart).format("YYYY/MM/DD-HH:mm:ss")}
-                          </td>{" "}
-                          <td>{moment(items.bidEnd).format("YYYY/MM/DD-HH:mm:ss")}</td>
-                          <td>{items.startingBid}</td>
-                          <td>
-                            <Button 
-                          onClick={() => showWinnerModal(items)}
-                          >
-                                View Winner
-                            </Button>
-                            <Modal
-                    title="Winner"
-                    visible={isWinnerModalVisible}
-                    onOk={handleCancel}
-                    onCancel={handleCancel}
-                  >
-                    {/* <p>Winner:{bidData[0].bidder.username} </p> */}
-                  </Modal>
-                          </td>
-                        </tr>
-                      );
-                }
+            if (items.status === "approved") {
+              if (currentDate > new Date(items.bidEnd)) {
+                return (
+                  <tr>
+                    <td>{items._id}</td> <td>{items.itemName}</td>
+                    <td>{items.description}</td>
+                    <td>
+                      <Image
+                        width={200}
+                        src={"http://localhost:8080/" + items.image}
+                      />
+                    </td>
+                    <td>
+                      {moment(items.created).format("YYYY/MM/DD-HH:mm:ss")}
+                    </td>
+                    <td>
+                      {moment(items.bidStart).format("YYYY/MM/DD-HH:mm:ss")}
+                    </td>{" "}
+                    <td>
+                      {moment(items.bidEnd).format("YYYY/MM/DD-HH:mm:ss")}
+                    </td>
+                    <td>{items.startingBid}</td>
+                    <td>
+                      <Button onClick={() => showWinnerModal(items)}>
+                        View Winner
+                      </Button>
+                      <Modal
+                        title="Winner"
+                        visible={isWinnerModalVisible}
+                        onOk={handleCancel}
+                        onCancel={handleCancel}
+                      >
+                        <p>Winner: {items.bids[0].bidder.username} </p>
+                        <p>Email: {items.bids[0].bidder.email}</p>
+                        <p>Phone: {items.bids[0].bidder.phone}</p>
+                      </Modal>
+                    </td>
+                  </tr>
+                );
+              }
             }
-            
-            
           })}
         </tbody>
       </Table>
